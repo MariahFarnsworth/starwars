8c533c6a43394b8f25871cbb3e6b777a00bf9e9f
MariahFarnsworth <47674407+MariahFarnsworth@users.noreply.github.com>
2019-03-02 21:51:00 -0500

    added styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -26,13 +26,13 @@ class Card extends React.Component{
    
         if(this.state.toggle === false){
             return(
-                <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+                <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="tc bg-moon-gray dit w5 h5 br3 ma2 shadow-5">
                     <h2>{this.props.name}</h2>
                 </div>
             );
         }else{
             return(
-                <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+                <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="tc bg-moon-gray dit w5 br3 h5 ma2 shadow-5 grow">
                 <h2>{this.props.name}</h2>
                 <p>Height: {this.props.height}</p>
                 <p>Mass: {this.props.mass}</p>
